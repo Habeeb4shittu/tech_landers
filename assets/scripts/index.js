dac1c8ca6692840ede696139d9a254dfe493c6de
Habeeb <habeeb4shittu@gmail.com>
2022-10-23 22:41:24 +0100

    index.html

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,10 @@
 $(".toggler").click(function () {
     $("nav").toggleClass("active")
+    if ($("nav").hasClass("active")) {
+        $(".toggler .bar #three-bar").removeClass("fa fa-bars").addClass("fas fa-times")
+        $(".toggler .bar #three-bar").addClass("close")
+    } else {
+
+        $(".toggler .bar #three-bar").removeClass("fas fa-times").addClass("fa fa-bars")
+    }
 })
