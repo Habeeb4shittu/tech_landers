9ace99ede8ee1f32bd5cc87b20e8b7bde8e3c000
Habeeb <habeeb4shittu@gmail.com>
2022-10-23 21:36:14 +0100

    index.html

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,3 @@
-$("#bars").click(function () {
-    $(".mobile-links").toggleClass("active")
-    $("#close").click(function () {
-        $(".mobile-links").removeClass("active")
-    })
+$(".toggler").click(function () {
+    $("nav").toggleClass("active")
 })
-ScrollReveal().reveal('.brand', { delay: 500, origin: 'left', distance: '200%' })
-ScrollReveal().reveal('.toggler', { delay: 500, origin: 'right', distance: '200%' })
-ScrollReveal().reveal('.home-link', { delay: 600, origin: 'top', distance: '200%', ease: 'ease-in' })
-ScrollReveal().reveal('.about-link', { delay: 700, origin: 'top', distance: '200%' })
-ScrollReveal().reveal('.services-link', { delay: 750, origin: 'left', distance: '200%' })
-ScrollReveal().reveal('.contact-link', { delay: 800, origin: 'bottom', distance: '200%' })
